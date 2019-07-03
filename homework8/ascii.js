b859539cb79897b36185e590d1d95fbfce3254d4
tushgee <tushgee@gmail.com>
2019-07-03 12:24:09 -0500

    start stop

--- homework8/ascii.js
+++ homework8/ascii.js
@@ -2,6 +2,12 @@
 let timer = null;
 let frameIndex = 0;
 let frames;
+
+
+//Tiny (7pt), Small (10pt), Medium (12pt), Larg e (16pt), Extra Large (24pt), XXL (32pt)
+
+
+
 function setAnimation() {
     const animation = document.getElementById("animationId").value;
     if (animation.toUpperCase()!="BLANK"){
@@ -9,7 +15,8 @@ function setAnimation() {
         document.getElementById("myTxtStage").value = ANIMATIONS[animation];
         frames = ANIMATIONS[animation].split("=====\n");
         document.getElementById("myTxtStage").value = frames[frameIndex];
-        if (frameIndex<frames.length-1) frameIndex++
+        if (frameIndex<frames.length-1) 
+          frameIndex++
         else frameIndex = 0;
 
     }
@@ -18,14 +25,21 @@ function setAnimation() {
 function animate() {
 	if (timer === null) {
 		timer = setInterval(setAnimation, 250);
-	} else {
-        clearInterval(timer);
-        timer = null;
-      //  document.body.style.backgroundColor = "red";
-    }
+	} 
+      
+}
+
+function stop() {
+	if (timer) {
+    clearInterval(timer);
+    timer = null;
+    //document.getElementById("myTxtStage").value = "";
+  }
 }
 
 window.onload = function() {
-  document.getElementById('btnStart').onclick = animate();
+  document.getElementById('btnStart').onclick = animate;
+  document.getElementById('btnStop').onclick = stop;
 }
+
 
