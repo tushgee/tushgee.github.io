b61034feae80488a64f91c7fad011b2bcba9c6aa
tushgee <tushgee@gmail.com>
2019-07-03 10:17:57 -0500

    animating

--- homework8/ascii.js
+++ homework8/ascii.js
@@ -1,9 +1,31 @@
 "use strict";
-
+let timer = null;
+let frameIndex = 0;
+let frames;
 function setAnimation() {
     const animation = document.getElementById("animationId").value;
     if (animation.toUpperCase()!="BLANK"){
       //  document.getElementById("myTxtStage").value = animation.toUpperCase();
         document.getElementById("myTxtStage").value = ANIMATIONS[animation];
+        frames = ANIMATIONS[animation].split("=====\n");
+        document.getElementById("myTxtStage").value = frames[frameIndex];
+        if (frameIndex<frames.length-1) frameIndex++
+        else frameIndex = 0;
+
     }
 }
+
+function animate() {
+	if (timer === null) {
+		timer = setInterval(setAnimation, 250);
+	} else {
+        clearInterval(timer);
+        timer = null;
+      //  document.body.style.backgroundColor = "red";
+    }
+}
+
+window.onload = function() {
+  document.getElementById('btnStart').onclick = animate();
+}
+
