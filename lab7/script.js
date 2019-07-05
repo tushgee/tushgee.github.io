0bc818dfaae027ad020acf52679e23c9103e00b0
tushgee <tushgee@gmail.com>
2019-07-05 15:36:54 -0500

    account

--- lab7/script.js
+++ lab7/script.js
@@ -1,1 +1,35 @@
-"use strict";
+let accountInfoList=[];
+
+let Account = function(){
+    "use strict";
+    let name;
+    let balance;  
+
+    function newAccount(nm="Tushig", bal=500000){
+        name = nm;
+        balance = bal;
+    }
+
+    return {
+        createAccount: function (name, bal){
+            newAccount(name, bal);
+        },
+        getName: function(){
+            return name;
+        }
+    }
+};
+
+function accountHandler(){
+    acc = Account();
+    acc.createAccount("Naya",150000);
+    console.log(acc.getName());
+    
+    //accountInfoList.push(acc.createAccount());
+    //console.log(accountInfoList[0].getName());
+}
+
+window.onload = function() {
+    document.getElementById('btnSubmit').onclick = accountHandler;
+  }
+  
