adad390755d9528470b65719d755d2782511a154
tushgee <tushgee@gmail.com>
2019-07-07 13:42:23 -0500

    upgrade to jquery hint code

--- homework9/fifteen.js
+++ homework9/fifteen.js
@@ -1,26 +1,27 @@
-init = function() {
-    var puzzleArea = document.getElementById('puzzlearea');
-    var divs = puzzleArea.getElementsByTagName("div");
+$(function(){
+    var puzzleArea = $('#puzzlearea');
+    var divs = $("#puzzlearea div");
+    console.log(divs);
       
     // initialize each piece
     for (var i=0; i< divs.length; i++) {
-        var div = divs[i];
+        var div = $(divs[i]);
         
         // calculate x and y for this piece
         var x = ((i % 4) * 100) ;
         var y = (Math.floor(i / 4) * 100) ;
 
         // set basic style and background
-        div.className = "puzzlepiece";
-        div.style.left = x + 'px';
-        div.style.top = y + 'px';
-        div.style.backgroundImage = 'url("background.jpg")';
-        div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
-        
+        div.css({"left": x + 'px', 
+                 "top": y + 'px', 
+                 "backgroundImage": 'url("background.jpg")', 
+                 "backgroundPosition": -x + 'px ' + (-y) + 'px' })
+            .addClass("puzzlepiece");
+
         // store x and y for later
         div.x = x;
         div.y = y; 
-    }        
-};
+    }    
 
-window.onload = init;
+});
+
